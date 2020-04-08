1f8212c72864a536ba330d0366c2533858f9e644
Derick Silva <dericksilva@live.com>
2020-04-08 16:30:57 -0300

    continuação todo list

--- Lista de Tarefas (to do)/assets/js/script.js
+++ Lista de Tarefas (to do)/assets/js/script.js
@@ -1,30 +1,46 @@
-let data = [{
-    id: 1,
-    title: "Estudar HTML"
-},{
-    id: 2,
-    title: "Estudar CSS"
-},{
-    id: 3,
-    title: "Estudar JavaScript"
-},{
-    id: 4,
-    title: "Estudar PHP"
-}]
+let data = []
 
-data.forEach(task => {
-    let li = document.createElement('li')
-    li.innerHTML = `
-                    <input type="checkbox" name="" id="task-${task.id}">
-                    <label for="task-${task.id}">${task.title}</label>`
+function renderTodo(){
 
-    li.querySelector('input').addEventListener("change", e =>{
-        if(e.target.checked) {
-            li.classList.add('complete')
-        }else{
-            li.classList.remove('complete')
-       } 
+    document.querySelector('.todo').innerHTML = ""
+
+    data.forEach(task => {
+        let li = document.createElement('li')
+        li.innerHTML = `
+            <input type="checkbox" name="" id="task-${task.id}">
+            <label for="task-${task.id}">${task.title}</label>
+            <button type="button">x</button>    
+            `
+
+        li.querySelector('input').addEventListener("change", e =>{
+            if(e.target.checked) {
+                li.classList.add('complete')
+            }else{
+                li.classList.remove('complete')
+        } 
+        })
+
+        li.querySelector('button').addEventListener('click', e => {
+            console.log('Voce vai deletar esse item')
+        })
+
+        document.querySelector('.todo').append(li)
     })
 
-    document.querySelector('.todo').append(li)
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+    if(e.key === 'Enter'){
+        console.log(e.target.value)
+        data.push({
+            id: data.length+1,
+            title: e.target.value
+        })
+
+        e.target.value = ""
+
+        renderTodo()
+    }
 })
+
+renderTodo()
